6158b0cd5fa0a969d38ddc4e208a0625373a4bcd
Lucas Martins <lucas.martins.fsa@gmail.com>
2022-04-22 09:30:20 -0300

    componentes

--- src/views/cadastro/consulta.js
+++ src/views/cadastro/consulta.js
@@ -1,6 +1,11 @@
 import React from "react";
-import ProdutoService from '../../app/produtoService'
+
+import Card  from '../../components/card';
+import ProdutosTable from './produtosTable';
+
+import ProdutoService from '../../app/produtoService';
 import {withRouter} from 'react-router-dom'
+
 
 class ConsultaProdutos extends React.Component{
 
@@ -30,48 +35,12 @@ class ConsultaProdutos extends React.Component{
 
     render(){
         return(
-            <div className="card">
-                <div className="card-header">
-                    Consulta de Produtos
-                </div>
-
-                <div className="card-body"> 
-
-                    <table className="table table-hover">
-                        <thead>
-                            <tr>
-                                <th>Nome</th> 
-                                <th>SKU</th>                                                       
-                                <th>Preço</th>
-                                <th>Fornecedor</th>
-                                <th>Anexo</th>
-                                <th>Ações</th>
-                            </tr>
-
-                        </thead>
-                        <tbody>
-                            {this.state.produtos.map( (produto, index) => {
-                                return (
-                                    <tr key={index}> 
-                                        <th>{produto.nome}</th>                                       
-                                        <th>{produto.sku}</th>                                                                
-                                        <th>{produto.preco}</th>
-                                        <th>{produto.fornecedor}</th>
-                                        <th>{produto.anexo}</th>
-                                        <th>
-                                            <button onClick={() => this.preparaEditar(produto.sku)} className="btn btn-primary">Editar</button>
-                                            <button onclick={() => this.deletar(produto.sku)} className="btn btn-danger">Remover</button>
-                                        </th>
-                                    </tr>
-                                )
-
-                                }) 
-                            }
-                        </tbody>
-
-                    </table>
-                </div>
-            </div>
+            <Card header="Consulta de Produtos">
+                <ProdutosTable produtos={this.state.produtos} 
+                    editarAction={this.preparaEditar}
+                    deLetarAction={this.deletar}/>                
+                
+            </Card>
         )
     }
 }
---
+++ src/views/cadastro/produtosTable.js
@@ -0,0 +1,40 @@
+import React from "react";
+
+export default (props) => (
+
+        <table className="table table-hover">
+            <thead>
+                <tr>
+                    <th>Nome</th> 
+                    <th>SKU</th>                                                       
+                    <th>Preço</th>
+                    <th>Fornecedor</th>
+                    <th>Anexo</th>
+                    <th>Ações</th>
+                </tr>
+
+            </thead>
+            <tbody>
+                {props.produtos.map( (produto, index) => {
+                    return (
+                        <tr key={index}> 
+                            <th>{produto.nome}</th>                                       
+                            <th>{produto.sku}</th>                                                                
+                            <th>{produto.preco}</th>
+                            <th>{produto.fornecedor}</th>
+                            <th>{produto.anexo}</th>
+                            <th>
+                                <button onClick={() => props.editarAction(produto.sku)} 
+                                    className="btn btn-primary">Editar</button>
+                                <button onclick={() =>props.deLetarAction(produto.sku)} 
+                                    className="btn btn-danger">Remover</button>
+                            </th>
+                        </tr>
+                    )
+
+                    }) 
+                }
+            </tbody>
+
+         </table>
+)
